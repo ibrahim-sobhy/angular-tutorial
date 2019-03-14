03f96879d7200483ca5d7f83f5f11c92dcbad720
Ibrahim <ibrahim.sobhy.cse07@gmail.com>
2019-03-14 09:15:12 +0100

    initial commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe Test title','Recipe Long description', 'https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1024/landscape-1503418862-chicken-thighs-delish.jpg?resize=1200:*'),
+    new Recipe('Recipe Test title','Recipe Long description', 'https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1024/landscape-1503418862-chicken-thighs-delish.jpg?resize=1200:*')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
